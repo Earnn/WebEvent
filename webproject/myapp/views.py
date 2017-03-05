0ea036aba3e99a6c115a7a2c5f6cabf18a19e165
EARN <EARN>
2017-03-05 18:09:48 +0700

    sample commit

--- webproject/myapp/views.py
+++ webproject/myapp/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from models import Person, Image
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {'key': "value" })
+	image_list = Image.objects.all()
+
+	return render(request, 'home.html', {
+			'image_list': image_list,
+			
+		
+			 })
 
 def base(request):
 	return render(request, 'base.html', {'key': "value" })
